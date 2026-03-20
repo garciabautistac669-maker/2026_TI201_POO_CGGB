5f12d6b487538407b82838542bbb989074d0e260
garciabautistac669@gmail.com <carlo@CarlosGerardo>
2026-03-20 10:24:59 -0600

    Se definio la clase automovil

---
+++ src/Tema_2_Clases_And_Objetos/Automovil.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Tema_2_Clases_And_Objetos;
+
+/**
+ *
+ * @author carlo
+ */
+public class Automovil {
+    
+    private String = marca;
+}
