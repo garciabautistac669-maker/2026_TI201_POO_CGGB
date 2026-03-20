380fd5052a2d6b3a7c06a4f7ea87ba74a2d9c3a8
garciabautistac669@gmail.com <carlo@CarlosGerardo>
2026-03-20 09:48:03 -0600

    Se agregaron las clases Exeption y Messaje

---
+++ src/Tema_2_Clases_And_Objetos/ExceptionTI201.java
@@ -0,0 +1,16 @@
+/*
+ * 2026 ITESS-TICS
+ * Periodo: Enero-Junio
+ * Asignatura: programación Orientada a Objetos
+ * Docente: Fracisco javier Montecillo Puente
+ * Descripción: Tema 2: Clases y Objetos
+                2.9 Manejo de excepciones 
+ * Programador: CGGB
+ * Fecha: 20 febrero 2026
+ *
+ */
+package Tema_2_Clases_And_Objetos;
+
+public class ExceptionTI201 {
+    
+}
